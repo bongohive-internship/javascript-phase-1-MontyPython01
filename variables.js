11a1ebe6458f6950de7c6e435be0517b4168ae92
MontyPython01 <51463362+MontyPython01@users.noreply.github.com>
2019-06-15 23:40:56 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,1 +1,12 @@
 // you solution should be here
+var birthYear = 1998;
+var futureYear = 2019;
+var age=futureYear-birthYear; 
+console.log('I will be either' + age + 'or' + (age - 1));
+
+var age = 21;
+var maxAge = 100;
+var numPerDay = 2;
+var totalNeeded = (numPerDay * 365) * (maxAge - age);
+var message = 'You will need' + totalNeeded + 'glasses of water to last you until the ripe age of ' + maxAge;
+console.log(message);
